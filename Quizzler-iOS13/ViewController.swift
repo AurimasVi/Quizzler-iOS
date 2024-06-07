598c7d5c0d826c8978380d4cddb5a9b08732754c
AurimasVi <aurimas.vidutis@gmail.com>
2024-06-07 16:18:23 +0300

    changed question type to struct

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -17,18 +17,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerLabel: UILabel!
     
     let quizzQuestions = [
-        ["1+1 = 2?", "True"],
-        ["1+2 = 3?", "True"],
-        ["1+4 = 2?", "False"],
-        ["2+2 = 4?", "True"]
-    ]
+        Question(q: "A slug's blood is green.", a: "True"),
+                Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+                Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+                Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+                Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+                Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+                Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+                Question(q: "Google was originally called 'Backrub'.", a: "True"),
+                Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+                Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+                Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+                Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+
+ ]
+    
     var questionNumber = 0
     
     @IBAction func answerBtnPressed(_ sender: UIButton) {
        
         progressBar.progress = Float(questionNumber + 1) / Float(quizzQuestions.count)
         
-        if quizzQuestions[questionNumber][1] == sender.titleLabel?.text && sender.titleLabel?.text != "Restart"  {
+        if quizzQuestions[questionNumber].answer == sender.titleLabel?.text && sender.titleLabel?.text != "Restart"  {
             answerLabel.text = "CORRECT"
             answerLabel.textColor = UIColor.green
             answerLabel.alpha = 1
@@ -76,7 +86,7 @@ class ViewController: UIViewController {
     
     func updateQuestion() {
         
-        questionLabel.text = quizzQuestions[questionNumber][0]
+        questionLabel.text = quizzQuestions[questionNumber].text
     }
     
     override func viewDidLoad() {
